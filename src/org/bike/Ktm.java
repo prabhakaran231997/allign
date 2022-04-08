3b78827cdf53001c783cba514a2aa52612d3ea12
prabhakaran231997 <prabhakarancivil23@gmail.com>
2022-04-08 16:29:11 +0530

    master done

---
+++ src/org/bike/Ktm.java
@@ -0,0 +1,15 @@
+package org.bike;
+
+public class Ktm  {
+
+	public void add1() {
+		System.out.println("add1");
+	}
+	
+
+	public static void main(String[] args) {
+		Ktm u = new Ktm();
+		u.add1();
+		
+	}
+}
